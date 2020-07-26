fde907a49b72c74eb58ea4ebc143cf77cf41d1c8
Kyle <kylehorton33@gmail.com>
2020-07-26 11:38:39 -0400

    api call for movies, not working

--- components/list.js
+++ components/list.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function MovieList() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/movies/', {
+      method: 'GET',
+      headers: {
+        'Authorization' : `Token a4db63b236737f5d5fe06163a57b83a797ab55e7`
+      }
+    })
+    .then( resp => resp.json())
+    .then( jsonResp => setMovies(jsonResp) )
+    .catch( err => console.log(err));
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>This will be a list</Text>
+      <FlatList 
+        data={movies}
+        renderItem={({item}) => (
+          <Text key={item.id}>{item.title}</Text>
+        )}
+      
+      />
     </View>
   );
 }
